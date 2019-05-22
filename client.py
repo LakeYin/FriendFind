7ca5ca893a07ca1f21f9b85bd62b5cf47ec2d939
LakeYin <la11010e@gmail.com>
2019-05-21 23:19:13 -0400

    Updated to a bot system in order to comply with Discord TOS

--- client.py
+++ client.py
@@ -1,9 +1,21 @@
 import discord
 
-token_file = (“token.txt”, “r”) # Retrieved from browser local storage
+token_file = open("token.txt", "r") # Retrieved from browser local storage
 
-TOKEN_AUTH = file.readline()
+TOKEN_AUTH = token_file.readline()
 
 client = discord.Client()
 
-client.run(TOKEN_AUTH, bot=False)
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN_AUTH)
