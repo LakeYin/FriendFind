4824b5a88494bfd951133a104145d8d21deb809b
LakeYin <la11010e@gmail.com>
2019-05-28 00:46:55 -0400

    Added code to create a list of all of the target user's messages

--- client.py
+++ client.py
@@ -1,10 +1,13 @@
 import discord
 
-token_file = open("token.txt", "r") # Retrieved from browser local storage
+token_file = open("token.txt", "r") #auth token
 
 TOKEN_AUTH = token_file.readline()
+token_file.close()
 
 client = discord.Client()
+
+running = False
 
 @client.event
 async def on_ready():
@@ -15,7 +18,24 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('!~find') and !running:
+		running = True
+        await message.author.send('Finding you friends...')
+		find_friends(message.author, message.guild)
+		running = False
+		
+	if message.content.startswith('!~find') and running:
+        await message.channel.send('Currently busy!')
+		
+	if message.content.startswith('!~help'):
+        await message.channel.send('Type ```!~find``` into this channel to let me find friends for you! Only one person can run this at a time.')
+		
+def find_friends(user, guild):
+	user_messages = []
 
+	for channel in guild.text_channels:
+		async for message in channel.history(limit=200):
+			if message.author != client.user and message.author == user:
+				user_messages.add(message)
+				
 client.run(TOKEN_AUTH)
