e27dfa6371d3cb31c1bd58ca1a28097ca8884895
LakeYin <la11010e@gmail.com>
2019-06-01 17:14:30 -0400

    Implemented code to convert the list of a user's messages into a list of numpy arrays where words are replaced with ints

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import discord
 import status
+import numpy
 
 token_file = open("token.txt", "r") #auth token
 
@@ -30,8 +31,12 @@ async def on_message(message):
         await message.channel.send('Type ```!~find``` into this channel to let me find friends for you! Only one person can run this at a time.')
 		
 async def find_friends(user, guild):
+	word_list = []
+	word_map = {}
+	
 	user_messages = []
-
+	user_ints = []
+	
 	for channel in guild.text_channels:
 		try:
 			async for message in channel.history().filter(lambda m: not m.author.bot and not m.attachments): #filters out messages from bots and messages with attachments
@@ -41,6 +46,27 @@ async def find_friends(user, guild):
 		except:
 			print("Could not view " + channel.name)
 				
-	print(user_messages)
+	#print(user_messages)
+	
+	def get_number(word): # maps words to an integer, returns integer if it already exists in the dictionary
+		if word in word_map:
+			return word_map[word];
+
+		word_index = len(word_list)
+		word_list.append(word)
+		word_map[word] = word_index
+		
+		return word_index
+		
+	for message in user_messages: # converts words into lists of integers
+		new_message = message.split()
+		message_to_ints = []
+		
+		for word in new_message:
+			message_to_ints.append(get_number(word))
+			
+		user_ints.append(numpy.array(message_to_ints, dtype = int))
+			
+	#print(user_ints)
 				
 client.run(TOKEN_AUTH)
