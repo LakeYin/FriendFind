1ed33ff8b59d04a55e84a9fdbf5530d160e7c93d
LakeYin <la11010e@gmail.com>
2019-06-05 02:58:43 -0400

    Moved some functions to a new file

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 import discord
-import helper
-import numpy
+import helper, finder
 
 token_file = open("token.txt", "r") #auth token
 
@@ -21,7 +20,7 @@ async def on_message(message):
     if message.content.startswith('!~find') and not helper.running:
         helper.running = True
         await message.author.send('Finding you friends...')
-        await find_friends(message.author, message.guild)
+        await finder.find_friends(message.author, message.guild)
         helper.running = False
 		
     if message.content.startswith('!~find') and helper.running:
@@ -29,37 +28,5 @@ async def on_message(message):
 		
     if message.content.startswith('!~help'):
         await message.channel.send('Type ```!~find``` into this channel to let me find friends for you! Only one person can run this at a time.')
-		
-async def find_friends(user, guild):
-	user_messages = []
-	other_messages = {}
-	
-	for member in guild.members:
-		if not member.bot and not member == user:
-			other_messages[member.id] = []
-	
-	for channel in guild.text_channels:
-		try:
-			async for message in channel.history().filter(lambda m: not m.author.bot and not m.attachments): #filters out messages from bots and messages with attachments
-				if message.author == user:
-					user_messages.append(message.clean_content)
-					
-				elif message.author.id in other_messages:
-					other_messages[message.author.id].append(message.clean_content)
-		
-		except:
-			print("Could not view " + channel.name)
-				
-	#print(user_messages)
-	
-	other_messages = dict((id, messages) for (id, messages) in other_messages.items() if len(messages) >= 10)
-	print(other_messages)
-	
-	user_ints = helper.messages_to_ints(user_messages)
-	#print(user_ints)
-	
-def clear_words():
-	helper.word_list.clear()
-	helper.word_map.clear()
 
 client.run(TOKEN_AUTH)
---
+++ finder.py
@@ -0,0 +1,34 @@
+import discord
+import helper
+
+async def find_friends(user, guild):
+	user_messages = []
+	other_messages = {}
+	
+	for member in guild.members:
+		if not member.bot and not member == user:
+			other_messages[member.id] = []
+	
+	for channel in guild.text_channels:
+		try:
+			async for message in channel.history().filter(lambda m: not m.author.bot and not m.attachments): #filters out messages from bots and messages with attachments
+				if message.author == user:
+					user_messages.append(message.clean_content)
+					
+				elif message.author.id in other_messages:
+					other_messages[message.author.id].append(message.clean_content)
+		
+		except:
+			print("Could not view " + channel.name)
+				
+	#print(user_messages)
+	
+	other_messages = dict((id, messages) for (id, messages) in other_messages.items() if len(messages) >= 10)
+	#print(other_messages)
+	
+	user_ints = helper.messages_to_ints(user_messages)
+	#print(user_ints)
+	
+def clear_words():
+	helper.word_list.clear()
+	helper.word_map.clear()
