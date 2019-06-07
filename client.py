ffe16672eab298fdb894459b316bd7c47e1d137a
LakeYin <la11010e@gmail.com>
2019-06-07 02:45:08 -0400

    Added logging, changed the machine learning a bit, and changed to a mention based index

--- client.py
+++ client.py
@@ -1,5 +1,12 @@
 import discord
 import helper, finder
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.ERROR)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 token_file = open("token.txt", "r") #auth token
 
@@ -23,29 +30,29 @@ async def on_message(message):
 		helper.running = True
 		friend_values = {}
 		
-		await message.author.send('Finding you friends...')
-		await message.channel.send('Running friend analysis...')
+		await message.author.send("Finding you friends... (this will take a while, don't be afraid if I go offline!)")
+		await message.channel.send("Running friend analysis... (this will take a while, don't be afraid if I go offline!)")
 		
 		friend_values = await finder.find_friends(message.author, message.guild)
 		
-		ordered_list = sorted(friend_values.items(), key=lambda x: x[1], reverse=True)
+		ordered_list = sorted(friend_values.items(), key=lambda x: x[1], reverse=True) # creates a lost of tuples
 		
 		report = "Your top 5 most similar people:\n\n"
 		
 		i = 0
 		while i < REPORT_COUNT and i < len(ordered_list):
-			report += "<@" + str(ordered_list[i][0]) + "> : " + '{:.1%}'.format(ordered_list[i][1]) + "\n"
+			report += ordered_list[i][0] + " : " + '{:.1%}'.format(ordered_list[i][1]) + "\n"
 			i += 1
 		
 		await message.author.send(report)
-		await message.channel.send('Analysis complete!')
+		await message.channel.send("Analysis complete!")
 		
 		helper.running = False
 		
-	if message.content.startswith('!~find') and helper.running:
-		await message.channel.send('Currently busy!')
+	if message.content.startswith("!~find") and helper.running:
+		await message.channel.send("Currently busy!")
 		
-	if message.content.startswith('!~help'):
-		await message.channel.send('Type `!~find` into this channel to let me find friends for you! Only one person can run this at a time.')
-
+	if message.content.startswith("!~help"):
+		await message.channel.send("Type `!~find` into this channel to let me find friends for you! Only one person can run this at a time.")
+		
 client.run(TOKEN_AUTH)
--- finder.py
+++ finder.py
@@ -1,6 +1,8 @@
 import discord
 import helper
 import numpy
+numpy.random.seed(9999)
+
 import tensorflow
 from tensorflow import keras
 
@@ -15,37 +17,41 @@ async def find_friends(user, guild):
 	
 	for member in guild.members:
 		if not member.bot and not member == user:
-			other_messages[member.id] = []
+			other_messages[member.mention] = []
 	
 	for channel in guild.text_channels:
 		try:
-			async for message in channel.history(limit=1000000).filter(lambda m: not m.author.bot and not m.attachments): #filters out messages from bots and messages with attachments
+			async for message in channel.history(limit=5000).filter(lambda m: not m.author.bot and not m.attachments): #filters out messages from bots and messages with attachments
 				if message.author == user:
 					user_messages.append(message.clean_content)
 					
-				elif message.author.id in other_messages:
-					other_messages[message.author.id].append(message.clean_content)
-		
+				elif message.author.mention in other_messages:
+					other_messages[message.author.mention].append(message.clean_content)
+			
+			print("Recorded " + channel.name)
+			
 		except:
 			print("Could not view " + channel.name)
 				
 	#print(user_messages)
 	
-	other_messages = dict((id, messages) for (id, messages) in other_messages.items() if len(messages) >= 10)
+	other_messages = dict((mention, messages) for (mention, messages) in other_messages.items() if len(messages) >= 10)
 	
 	user_ints = helper.messages_to_ints(user_messages)
 	user_ints = keras.preprocessing.sequence.pad_sequences(user_ints, value=helper.word_map["<PAD>"], padding='post', maxlen=2000) # max character count for a discord message is 2000
 	
-	for id, messages in other_messages.items():
-		other_ints[id] = helper.messages_to_ints(messages)
+	for mention, messages in other_messages.items():
+		other_ints[mention] = helper.messages_to_ints(messages)
 	
 	labels = numpy.array([1] * len(user_ints), dtype=int)
 	#print(user_ints)
 	
 	model = create_model(len(helper.word_map))
-	model.fit(user_ints, labels, epochs=40, verbose=0)
+	print("Fitting model")
+	model.fit(user_ints, labels, epochs=30, verbose=0)
 	
-	for id, ints in other_ints.items():
+	print("Making predictions")
+	for mention, ints in other_ints.items():
 		total_value = 0
 		count = 0
 		for int_message in ints: 
@@ -54,8 +60,8 @@ async def find_friends(user, guild):
 				count += 1
 				total_value += prediction
 			
-		friend_values[id] = total_value / count
-		
+		friend_values[mention] = total_value / count
+	
 	return friend_values
 		
 def create_model(vocab):
@@ -65,7 +71,7 @@ def create_model(vocab):
 	model.add(keras.layers.Dense(16, activation=tensorflow.nn.relu))
 	model.add(keras.layers.Dense(1, activation=tensorflow.nn.sigmoid))
 	
-	model.compile(optimizer='adadelta', loss='binary_crossentropy', metrics=['acc'])
+	model.compile(optimizer='adagrad', loss='binary_crossentropy', metrics=['acc'])
 	
 	return model	
 	
